8bdea8708def7aa0ed3666264f91b0114b265eff
Miguel Bastos <miguel.alexandre.bastos@gmail.com>
2025-03-06 19:37:27 +0000

    Added most features for MVP, system to populate, shuffle, deal, and Draw cards

--- Javascript/game.js
+++ Javascript/game.js
@@ -16,6 +16,8 @@ class Game{
         this.gameOver = false
         this.gameIntervalId
         this.gameLoopId = 1000/60
+
+        this.deck = new deck()
     }
     start(){
         this.gameScreen.style.width =  `${this.width}px`
@@ -23,6 +25,12 @@ class Game{
 
         this.startScreen.style.display = "none"
         this.gameScreen.style.display = "block"
+
+        console.log("Deck before shuffle", this.deck.poplateDeck())
+        console.log("Deck after shuffle", this.deck.shuffle())
+
+        let [playerHand, cpuHand] = this.deck.dealCards()
+        
 
         this.gameIntervalId = setInterval(()=> {
             this.gameLoop();},
@@ -48,5 +56,63 @@ class Game{
 
 
 class deck{
-    constructor(){}
+    constructor(){
+        this.deck = []
+    }
+    
+    poplateDeck(){
+        const colors = ["purple", "cyan", "pink", "orange"]
+        const values =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+        this.deck = [];
+
+        for(let i = 0; i < colors.length; i++){
+            for(let j = 0; j < values.length; j++){
+                let card = {
+                    color: colors[i],
+                    value: values[j]
+                }
+                this.deck.push(card)
+            }
+        }
+        return this.deck
+    }
+
+    shuffle(){
+        let shuffleCount = 0
+
+        while (shuffleCount < 100) {
+            let i = Math.floor(Math.random() * this.deck.length)
+            let j = Math.floor(Math.random() * this.deck.length)
+
+            let temp = this.deck[i]
+            this.deck[i]= this.deck[j]
+            this.deck[j] = temp
+
+            shuffleCount++
+        }
+        return this.deck
+    }
+    
+    dealCards(){
+        console.log("Dealing " + 8 + " cards")
+        
+
+        let playerHand = []
+        let cpuHand = []
+
+        for (let i = 0; i < 8; i++){
+            let newCard = this.deck.pop()
+            playerHand.push(newCard)
+        }
+
+        for (let j = 0; j < 8; j++){
+            let otherCard = this.deck.pop()
+            cpuHand.push(otherCard)
+        }
+        console.log("Player Hand after dealing:", playerHand)
+        console.log("CPU Hand after dealing:", cpuHand)
+        return [playerHand, cpuHand]
+
+    }
 }
